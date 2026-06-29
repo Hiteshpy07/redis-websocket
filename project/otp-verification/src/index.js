ec73b31188284023a77f6ba321252262e3b83587
Hitesh Kumar Nayak <kumarnayakhitesh@gmail.com>
2026-06-29 11:04:06 +0530

    added otp-generation

--- project/otp-verification/src/index.js
+++ project/otp-verification/src/index.js
@@ -12,7 +12,35 @@ app.get("/",async (req,res)=>{
     res.send(reply)
 })
 
+app.post("/getotp",async(req,res)=>{
+    const {phone}=req.body
+    const typeofphone=typeof phone
+    console.log(typeofphone)
+    // const phonelength=phone.length
+    console.log(phone)
+    if(phone ){
+        const otp=Math.floor(1000+Math.random()*9000)
+        await redis.set(phone,otp,"EX",300)
+        res.send({message:`OTP sent successfully to ${phone}`,otp:otp})
+    }else{
+        res.status(400).send({message:"Invalid phone number"})
+    }
+})
 
+app.get("/mongodb",async (req,res)=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL||'mongodb://localhost:27017');
+        if (mongoose.connection.readyState===1){
+
+            res.send("MongoD3 connected successfully");
+        }else{
+            res.send("MongoDB connection failed");
+        }
+        // res.send(mongoose.connection.readyState,mongoose.connection.name);
+    }catch(err){
+        res.status(500).send("MongoDB connection failed");
+    }
+})  
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
