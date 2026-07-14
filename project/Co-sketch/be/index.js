bdf2a016236d59303a907f6ea5c4857b61880910
Hitesh Kumar Nayak <kumarnayakhitesh@gmail.com>
2026-07-14 09:43:34 +0530

    working on redis pub-sub for chat section

---
+++ project/Co-sketch/be/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import Redis from 'ioredis';
+import { createServer } from 'http'; //new thing ? why used here?
+//Line 4 (createServer): Imports Node's native HTTP module. Socket.io cannot attach directly to an Express app wrapper; it needs a raw HTTP server instance to establish its persistent handshake.
+
+import { Server } from 'socket.io'; //useing websockets
+
+const app = express();
+app.use(cors({origin: '*'}));
+
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+//wrapping the ecpress server by a raw HTTP server instance to allow socket.io to work with it. The cors option allows cross-origin requests from any domain.
+
+const redis = new Redis('redis://localhost:6379'); // Connect to Redis server in docer, not direclty running it ,  so redis:// not http://
+const redisSub = redis.duplicate();//duplicate redis server for pubsub model 
+
+const CHAT_CHANNEL = 'CO_SKETCH_CHANNEL';
+
+
+
+//settig up the reis suscribe model, to listen any new data iin the chat channel
+async function setupRedisSubscription() {
+    await redisSub.subscribe(CHAT_CHANNEL);
+    //setup the duplicate server to the suscribe mode for the channel
+    redisSub.on('message',(channel,message)=>{
+        if (channel === CHAT_CHANNEL) //double check , if listening to corrext chaneel , there can be multiple chaneel runninng side by side
+            {
+                console.log(`Received message from Redis channel ${channel}: ${message}`);
+                const data = JSON.parse(message);//good practice he- always sent datat from BE to FE IN JSON FORMAT, EASE TO USE THERE
+                io.to(data.roomId).emit('receive_chat_message', {
+        user: data.user,
+        message: data.message
+      });
+    }
+    //io.to(data.roomId).emit(...): Takes the verified data object and pushes it down the WebSocket pipe to every browser instance currently sitting inside that specific room.
+  });
+}
+setupRedisSubscription().catch(console.error);
