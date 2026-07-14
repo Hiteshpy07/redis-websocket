794a8617304394a58a8028839e54c5af5e3fbad1
Hitesh Kumar Nayak <kumarnayakhitesh@gmail.com>
2026-07-14 10:08:05 +0530

    wrote core FE logic of socket connection

--- project/Co-sketch/fe/vite-project/src/App.jsx
+++ project/Co-sketch/fe/vite-project/src/App.jsx
@@ -1,8 +1,39 @@
-import { useState } from 'react'
-
+import { useState,useEffect} from 'react'
+import {io} from 'socket.io-client'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState('');
+  const [chatLog, setChatLog] = useState([]);
+
+  const roomId = "meeting-123"; 
+  const username = "Hitesh";
+
+
+  useEffect(() => {
+    // 1. Connect to our Node backend server
+    const newSocket = io('http://localhost:3001');
+    setSocket(newSocket);
+    newSocket.emit('join_room', roomId);
+
+    newSocket.on('receive_chat_message', (data) => {
+      setChatLog((prev) => [...prev, `${data.user}: ${data.message}`]);
+    });
+    return () => newSocket.disconnect();
+  }, []);
+
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    if (!message.trim() || !socket) return;
+    socket.emit('send_chat_message', {
+      roomId,
+      user: username,
+      message: message
+    });
+
+    setMessage(''); // Clear input box
+  };
 
   return (
    <>
